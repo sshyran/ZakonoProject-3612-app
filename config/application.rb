2eff53dede6245950097cbcb161b9f5395c16f45
Sergio Shyran <s.shyran@gmail.com>
2021-03-28 19:30:54 +0300

    Initialize alt/vendee

---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# TODO : add missing dep to decidim-initiatives/lib/decidim/initiatives/engine.rb
+# require "wicked_pdf"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module DevelopmentApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+    config.time_zone = "Europe/Paris"
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
+    
+    # This needs to be set for correct images URLs in emails
+    # DON'T FORGET to ALSO set this in `config/initializers/carrierwave.rb`
+    # config.action_mailer.asset_host = "https://your.server.url"
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
