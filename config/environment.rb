cacb1ee473f64791a85820d38fc5e5dfc25a6188
Sergio Shyran <s.shyran@gmail.com>
2021-03-28 19:30:50 +0300

    Initialize test-performance

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require "wicked_pdf"
+
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
+
+Rails.application.routes.draw do
+  authenticate :admin do
+    mount Sidekiq::Web => '/sidekiq'
+    # mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
+  mount Decidim::Core::Engine => '/'
+  # mount Decidim::Map::Engine => '/map'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
