2ee9f417f08c50c8b84bdad13a683cd09690d1f3
Sergio Shyran <s.shyran@gmail.com>
2021-03-28 19:30:30 +0300

    Initialize feature/stystem-dashboard

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "sidekiq/web"
+require "sidekiq-scheduler/web"
+
+Rails.application.routes.draw do
+
+  authenticate :admin do
+    mount Sidekiq::Web, at: "sidekiq", as: :sidekiq
+    mount PgHero::Engine, at: "pghero"
+    mount Split::Dashboard, at: "split"
+  end
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  mount Decidim::Core::Engine => "/"
+  # mount Decidim::Map::Engine => '/map'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
