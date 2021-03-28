a8e56bd71dda7bc04e79e34172b92dc1ee6ca0b2
Sergio Shyran <s.shyran@gmail.com>
2021-03-28 19:30:38 +0300

    Initialize helpscout-0.18

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+require "sidekiq/web"
+Rails.application.routes.draw do
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  mount Decidim::Core::Engine => '/'
+  # mount Decidim::Map::Engine => '/map'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
