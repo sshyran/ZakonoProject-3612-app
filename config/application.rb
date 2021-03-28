03eb3ed38ee00f9085ac7744e933240f9be8024a
Sergio Shyran <s.shyran@gmail.com>
2021-03-28 19:30:28 +0300

    Initialize translation/alt-cd44

---
+++ config/application.rb
@@ -0,0 +1,32 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module DecidimApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+    config.time_zone = "Paris"
+
+    # This needs to be set for correct images URLs in emails
+    # DON'T FORGET to ALSO set this in `config/initializers/carrierwave.rb`
+    config.action_mailer.asset_host = "https://participer.loire-atlantique.fr"
+
+    config.after_initialize do
+      if Decidim.geocoder.present?
+        Geocoder.configure(
+          lookup: :nominatim,
+          :timeout  => 30
+        )
+      end
+    end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
