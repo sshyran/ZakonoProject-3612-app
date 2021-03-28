9160739f3693ae40cbc1363f98a5005d2fc46302
Sergio Shyran <s.shyran@gmail.com>
2021-03-28 19:31:27 +0300

    Initialize release/0.23-stable

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,12 @@
+require "sentry-ruby"
+
+if Rails.application.secrets.dig(:sentry, :enabled)
+  Sentry.init do |config|
+    config.dsn = Rails.application.secrets.dig(:sentry, :dsn)
+    config.breadcrumbs_logger = [:active_support_logger]
+
+    # To activate performance monitoring, set one of these options.
+    # We recommend adjusting the value in production:
+    config.traces_sample_rate = ENV.fetch("SENTRY_SAMPLE_RATE", 0.5)
+  end
+end
